d625d6dcc49f60ddb1e8bc2199ce2c5680b88e41
Alejandro Marroquín <106278424+AlejandroMarroquin18@users.noreply.github.com>
2025-01-31 10:53:54 -0500

    Campos de facultad y unidad

--- quejas/models.py
+++ quejas/models.py
@@ -4,6 +4,8 @@ class Queja(models.Model):
     nombre = models.CharField(max_length=100)
     sede = models.CharField(max_length=100)
     codigo = models.CharField(max_length=100)
+    facultad = models.CharField(max_length=100, null=True)
+    unidad = models.CharField(max_length=100, null=True)
     tipo_de_acompanamiento = models.CharField(max_length=100)
     fecha = models.DateField()
     estado = models.CharField(max_length=100)
