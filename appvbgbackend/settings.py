128119ca4e64bfd29198a3057a1bbca9b0dbe529
Alejandro Marroquín <alejandro.marroquin@correounivalle.edu.co>
2025-10-26 22:51:09 -0500

    Update settings.py

--- appvbgbackend/settings.py
+++ appvbgbackend/settings.py
@@ -19,7 +19,8 @@ load_dotenv()
 
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
-GOOGLE_REDIRECT_URI = "postmessage"
+#GOOGLE_REDIRECT_URI = "postmessage"
+GOOGLE_REDIRECT_URI = "https://app-gestion-casos-vbg-frontend.vercel.app"
 
 
 ##Manejo de Cookies
