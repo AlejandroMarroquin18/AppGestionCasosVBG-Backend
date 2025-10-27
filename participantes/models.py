7f72a7d1fa3366337959b081a0b76949a2fd8f11
Alejandro Marroquín <alejandro.marroquin@correounivalle.edu.co>
2025-10-27 01:32:04 -0500

    arreglos

--- participantes/models.py
+++ participantes/models.py
@@ -6,7 +6,7 @@ class Participant(models.Model):
     email = models.EmailField()
     full_name = models.CharField(max_length=255)
     document_type = models.CharField(max_length=100)
-    document_number = models.CharField(max_length=100, unique=True)  # Aseguramos que sea único
+    document_number = models.CharField(max_length=100)  
     age = models.IntegerField()
     disability = models.CharField(max_length=100, null=True, blank=True)
     program = models.CharField(max_length=255, null=True, blank=True)  # Nuevo campo para el programa académico / dependencia
@@ -14,6 +14,9 @@ class Participant(models.Model):
     self_recognition = models.CharField(max_length=100, null=True, blank=True)  # Nuevo campo para el auto-reconocimiento
     terms_accepted = models.BooleanField(default=False, null=True, blank=True)  # Aceptación de los términos y condiciones
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    class Meta:
+        unique_together = ['workshop', 'document_number']
 
     def __str__(self):
         return self.full_name
